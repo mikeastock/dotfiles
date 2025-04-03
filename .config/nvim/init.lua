78d670e382cfa6cc9b2e090086b9a811b2e71747
Michael Stock <mikeastock@gmail.com>
2025-04-03 13:59:19 -0700

    Use config over setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,16 +168,20 @@ let g:user_debugger_dictionary = { '\.rb' : 'binding.pry', '\.tsx' : 'debugger' 
 
 -- Plugins
 
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -251,7 +255,7 @@ require("lazy").setup({
   -- workflow
   {
     "FooSoft/vim-argwrap",
-    setup = function()
+    config = function()
       nmap("<Leader>a", "<cmd>ArgWrap<CR>")
       vim.g.argwrap_tail_comma = true
     end,
@@ -267,7 +271,7 @@ require("lazy").setup({
   "junegunn/vim-easy-align",
   {
     "justinmk/vim-sneak",
-    setup = function()
+    config = function()
       nmap("f", "<Plug>Sneak_f")
       nmap("F", "<Plug>Sneak_F")
     end,
